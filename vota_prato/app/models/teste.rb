a6e756279b5650c40a09ff2f5030e8d080bfc9f0
wdmeida <wdmeida@gmail.com>
2016-04-23 20:32:33 -0300

    Teste de rota

--- vota_prato/app/models/teste.rb
+++
@@ -1,4 +0,0 @@
-#coding: utf-8
-class Teste < ActiveRecord::Base
-	validates_presence_of :nome, message: "Nome não preenchido."
-end
--- vota_prato/config/routes.rb
+++ vota_prato/config/routes.rb
@@ -1,4 +1,6 @@
 VotaPrato::Application.routes.draw do
+  
+  match 'ola' => 'ola_mundo#index', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
