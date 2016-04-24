da6263ddfdba2f5fa534a3f65712b6a7faf1d73c
wdmeida <wdmeida@gmail.com>
2016-04-23 22:26:17 -0300

    Criação actionshow

--- vota_prato/app/models/restaurante.rb
+++ vota_prato/app/models/restaurante.rb
@@ -1,19 +1,19 @@
 #coding: utf-8
 class Restaurante < ActiveRecord::Base
-  validates_presence_of :nome, :message => "deve ser preenchido"
-  validates_presence_of :endereco, :message => "deve ser preenchido"
-  validates_presence_of :especialidade, :message => "deve ser preenchido"
-  
-  validates_uniqueness_of :nome, :message => "nome ja cadastrado"
-  validates_uniqueness_of :endereco, :message => "endereco ja cadastrado"
-  
-  validate :primeira_letra_deve_ser_maiuscula
-  private
-  def primeira_letra_deve_ser_maiuscula
-    errors.add("nome", "primeira letra deve ser maiuscula") unless nome =~ /[A-Z].*/
-  end
-  
-  has_many :qualificacoes
-  has_and_belongs_to_many :pratos
-  has_many :comentarios, :as => :comentavel
+	  validates_presence_of :nome, :message => "deve ser preenchido"
+	  validates_presence_of :endereco, :message => "deve ser preenchido"
+	  validates_presence_of :especialidade, :message => "deve ser preenchido"
+	  
+	  validates_uniqueness_of :nome, :message => "nome ja cadastrado"
+	  validates_uniqueness_of :endereco, :message => "endereco ja cadastrado"
+	  
+	  validate :primeira_letra_deve_ser_maiuscula
+	  private
+	  def primeira_letra_deve_ser_maiuscula
+	    errors.add("nome", "primeira letra deve ser maiuscula") unless nome =~ /[A-Z].*/
+	  end
+	  
+	  has_many :qualificacoes
+	  has_and_belongs_to_many :pratos
+	  has_many :comentarios, :as => :comentavel
 end
